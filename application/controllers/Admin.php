003d9908fcfeedaa35e7d5f59c50b923c926e06d
Rhea <rhealauzon@gmail.com>
2015-02-19 16:34:04 -0800

    Quote validation implemented. Lab 5 completed. Signed-off-by:Rhea <rhealauzon@gmail.com>

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -38,8 +38,17 @@ class Admin extends Application {
     
     function present($quote)
     {
+        // format any errors
+        $message = '';
+        if (count($this->errors) > 0) 
+        {
+          foreach ($this->errors as $booboo)
+            $message .= $booboo . BR;
+        }
+        $this->data['message'] = $message;
+  
         
-        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id);
+        $this->data['fid'] = makeTextField('ID#', 'id', $quote->id, "Unique Quote Identifier, system-assigned", 10, 10, true);
         $this->data['fwho'] = makeTextField('Author', 'who', $quote->who);
         $this->data['fmug'] = makeTextField('Picture', 'mug', $quote->mug);
         $this->data['fwhat'] = makeTextArea('The Quote', 'what', $quote->what);
@@ -48,6 +57,50 @@ class Admin extends Application {
         $this->data['fsubmit'] = makeSubmitButton('Process Quote', "Click here to validate the quotation data", 'btn-success');
         $this->render();
   
+    }
+    
+    //process a quotation edit
+    function confirm() {
+        $record = $this->quotes->create();
+        
+        //Extract submitted fields
+        $record->id = $this->input->post('id');
+        
+        $record->who = $this->input->post('who');
+        
+        $record->mug = $this->input->post('mug');
+        
+        $record->what = $this->input->post('what');
+        
+        //validation
+        if (empty($record->who))
+        {
+            $this->errors[] = 'You must specify an author.';
+        }
+        
+        if (strlen($record->what) < 20)
+        {
+            $this->errors[] = 'A quotation must be at least 20 characters long.';
+        }
+        
+        if (count($this->errors) > 0)
+        {
+            $this->present($record);
+            return;
+        }
+        
+        
+        //Save the information in our database
+        if (empty($record->id))
+        {
+            $this->quotes->add($record);
+        }
+        else
+        {
+            $this->quotes->update($record);
+        }
+        
+        redirect('/admin');
     }
 
 }
